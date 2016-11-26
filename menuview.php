882b6a847e196d1a1f5a3d2068c14194ab014160
Stefan <moldovanst@yahoo.com>
2016-11-26 16:13:16 +0200

    Delete menuview.php

--- menuview.php
+++
@@ -1,9 +0,0 @@
-<div id="menu-bar">
-<ul>
-    <li><a href="<?php echo BASE_URL;?>index.php">Home</a></li>
-    <li><a href="<?php echo BASE_URL;?>aboutus">Despre noi</a></li>
-    <li><a href="<?php echo BASE_URL;?>articles">Blog</a></li>
-    <li><a href="<?php echo BASE_URL;?>shop">Produse</a></li>
-    <li><a href="#" id="contactt">Contact</a></li>
-</ul>
-</div>
